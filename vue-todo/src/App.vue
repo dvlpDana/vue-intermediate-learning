de5e87ee37332e07ef96b6a144db155e522d8fdf
DahnBee Shim <colleksql@8snippet.com>
2022-12-15 20:28:54 +0900

    Scafolding to make a todo-app

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TodoFooter />
+    <TodoHeader />
+    <TodoInput />
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    TodoFooter,
+    TodoHeader,
+    TodoInput,
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+</style>
